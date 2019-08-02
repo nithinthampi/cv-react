9ac2aad628bd9bd477b58802477f9dd966d706f2
Nithin Thampi <nthampi@walmart.com>
2019-08-02 11:15:43 +0530

    Move files to apache

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
-ssh -i /tmp/LightsailDefaultKey-ap-south-1.pem -o StrictHostKeyChecking=no bitnami@52.66.162.168 rm -rf test
-scp -i /tmp/LightsailDefaultKey-ap-south-1.pem -o StrictHostKeyChecking=no -rp ./build bitnami@52.66.162.168:~
+ssh -i /tmp/LightsailDefaultKey-ap-south-1.pem -o StrictHostKeyChecking=no bitnami@52.66.162.168 rm -rf build/*
+scp -i /tmp/LightsailDefaultKey-ap-south-1.pem -o StrictHostKeyChecking=no -rp ./build/* bitnami@52.66.162.168:~/build
+ssh -i /tmp/LightsailDefaultKey-ap-south-1.pem -o StrictHostKeyChecking=no bitnami@52.66.162.168 rm -rf htdocs/*
+ssh -i /tmp/LightsailDefaultKey-ap-south-1.pem -o StrictHostKeyChecking=no bitnami@52.66.162.168 mv build/* htdocs
